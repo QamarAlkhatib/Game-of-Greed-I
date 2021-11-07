33ef30c63a3658cf05e103b19589c12aa3eb9a5d
belelelolahi <belalelolahi003@gmail.com>
2021-11-07 16:18:15 +0200

    add methods of class banker

--- game_of_greed/banker.py
+++ game_of_greed/banker.py
@@ -1,2 +1,12 @@
+class Banker :
+    def __init__(self) -> None:
+          self.shelved = 0 
+          self.balance = 0 
+
+          
+
+    def  shelf(self,amount):
+          self.shelved +=amount
+
 
     
