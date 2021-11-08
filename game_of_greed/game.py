c620119a53a880931cf3f77501d2389d429202f4
belelelolahi <belalelolahi003@gmail.com>
2021-11-08 17:20:16 +0200

    rolling dice game

--- game_of_greed/game.py
+++ game_of_greed/game.py
@@ -41,7 +41,7 @@ rules={
 }      
 class Game():
     def __init__(self,roller=None):
-        # super.__init__(self.shelved,self.balance)
+        
         self.roller = roller
 
     def play(self):
@@ -66,22 +66,49 @@ class Game():
                 decide = input('Enter dice to keep (no spaces), or (q)uit: ')
                 score = 0
                 
-                # banked = object.bank()
+               
                
                 
                 if decide != 'q':
                     var = tuple(int(i) for i in decide) 
-                    # print(var)
+                   
                     score = GameLogic.calculate_score(var)
                     length = 6 -len(var)
                     print(f'You have {score} unbanked points and {length} dice remaining')
                     bank = input('(r)oll again, (b)ank your points or (q)uit ')
                     if bank == 'b':
-                        # object = Banker()
+                        
                         shelf = object.shelf(score)
                         
                         banked = object.bank()
                         roundBanked = object.bank()
                         print(f'You banked {score} points in round {counter}')
                         print(f'Total score is {banked} points')
+                    elif bank == 'q':
+                        print(f'Total score is {banked} points')
+                        print(f'Thanks for playing. You earned {banked} points')
+                        break
+                    
+
+                   
                         
+                        banked = banked
+
+
+                elif decide == 'q' and counter > 1:
+                   print(f'Total score is {banked} points')
+                   print(f'Thanks for playing. You earned {banked} points')
+                   break
+                else:
+                    print(f'Thanks for playing. You earned {banked} points')
+                    break
+                counter +=1
+            
+
+                
+
+
+if __name__ == '__main__':
+    cal = 5
+    game =Game(GameLogic.roll_dice)
+    game.play()
---
+++ tests/test_bank_first_two_round.py
@@ -0,0 +1,3 @@
+from tests.flow.flo import Flo
+def test_two_round_done():
+    Flo.test('tests/flow/bank_first_for_two_rounds.sim.txt')
